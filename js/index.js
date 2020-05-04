3aaac1ef2116c2eea8f3d7b2d82f7558468f2b7d
chalat.mo <ex-chalat.mo@cjdfintech.com>
2020-05-04 18:57:01 +0700

    mobile view

--- js/index.js
+++ js/index.js
@@ -1,58 +1,101 @@
 (function()
 {
-	var canvas = document.querySelector( "#canvas" );
-	var context = canvas.getContext( "2d" );
-	canvas.width = 280;
-	canvas.height = 280;
 	
+	var canvas,ctx;
+
+	var mouseX,mouseY,mouseDown=0;
+
+	var touchX,touchY;
+
 	var Mouse = { x: 0, y: 0 };
 	var lastMouse = { x: 0, y: 0 };
-	context.fillStyle="white";
-	context.fillRect(0,0,canvas.width,canvas.height);
-	context.color = "black";
-	context.lineWidth = 6;
-	context.lineJoin = context.lineCap = 'round';
-	debug();
-	// startup(); 
 
-	canvas.addEventListener( "mousemove", function( e ){
-		lastMouse.x = Mouse.x;
-		lastMouse.y = Mouse.y;
 
-		Mouse.x = e.pageX - this.offsetLeft;
-		Mouse.y = e.pageY - this.offsetTop;
+	function drawDot(ctx,x,y,size) {
+		r=0; g=0; b=0; a=255;
 
-	}, false );
+		ctx.fillStyle = "rgba("+r+","+g+","+b+","+(a/255)+")";
 
-	canvas.addEventListener( "mousedown", function( e ){
+		ctx.beginPath();
+		ctx.arc(x, y, size, 0, Math.PI*2, true); 
+		ctx.closePath();
+		ctx.fill();
+	} 
+
+	function clearCanvas(canvas,ctx) {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	}
+
+	function sketchpad_mouseDown() {
+		var canvas = document.querySelector( "#canvas" );
 		canvas.addEventListener( "mousemove", onPaint, false );
-	}, false );
+	}
 
-	canvas.addEventListener( "mouseup", function(){
+	function sketchpad_mouseUp() {
+		var canvas = document.querySelector( "#canvas" );
 		canvas.removeEventListener( "mousemove", onPaint, false );
-	}, false );
+	}
 
-	canvas.addEventListener( "touchmove", function( e ){
+	function sketchpad_mouseMove(e) { 
+
+		var canvas = document.querySelector( "#canvas" );
+		var context = canvas.getContext( "2d" );
+
+		context.color = "black";
+		context.lineWidth = 10;
+
 		lastMouse.x = Mouse.x;
 		lastMouse.y = Mouse.y;
 
 		Mouse.x = e.pageX - this.offsetLeft;
 		Mouse.y = e.pageY - this.offsetTop;
+	}
 
-	}, false );
+	function getMousePos(e) {
+		if (!e)
+			var e = event;
 
-	canvas.addEventListener( "touchstart", function( e ){
-		canvas.addEventListener( "touchmove", onPaint, false );
-		// canvas.addEventListener( "mousemove", onPaint, false );
-	}, false );
+		if (e.offsetX) {
+			mouseX = e.offsetX;
+			mouseY = e.offsetY;
+		}
+		else if (e.layerX) {
+			mouseX = e.layerX;
+			mouseY = e.layerY;
+		}
+	}
 
-	canvas.addEventListener( "touchend", function(){
-		canvas.removeEventListener( "touchmove", onPaint, false );
-		// canvas.removeEventListener( "mousemove", onPaint, false );
+	function sketchpad_touchStart() {
+		getTouchPos();
 
-	}, false );
+		drawDot(ctx,touchX,touchY,6);
 
-	var onPaint = function(){	
+		event.preventDefault();
+	}
+
+	function sketchpad_touchMove(e) { 
+		getTouchPos(e);
+
+		drawDot(ctx,touchX,touchY,6); 
+
+		event.preventDefault();
+	}
+
+
+	function getTouchPos(e) {
+		if (!e)
+			var e = event;
+
+		if(e.touches) {
+			if (e.touches.length == 1) { 
+				var touch = e.touches[0];
+				touchX=touch.pageX-touch.target.offsetLeft;
+				touchY=touch.pageY-touch.target.offsetTop;
+			}
+		}
+	}
+
+	function onPaint(){	
 		context.lineWidth = context.lineWidth;
 		context.lineJoin = "round";
 		context.lineCap = "round";
@@ -65,7 +108,30 @@
 		context.stroke();
 	};
 
+
+	function init() {
+		canvas = document.getElementById('canvas');
+
+		if (canvas.getContext)
+			ctx = canvas.getContext('2d');
+
+		if (ctx) {
+			// React to mouse events on the canvas, and mouseup on the entire document
+			canvas.addEventListener('mousedown', sketchpad_mouseDown, false);
+			canvas.addEventListener('mousemove', sketchpad_mouseMove, false);
+			window.addEventListener('mouseup', sketchpad_mouseUp, false);
+
+			// React to touch events on the canvas
+			canvas.addEventListener('touchstart', sketchpad_touchStart, false);
+			canvas.addEventListener('touchmove', sketchpad_touchMove, false);
+		}
+	}
+
 	function debug(){
+
+		canvas = document.getElementById('canvas');
+		context = canvas.getContext('2d');
+
         var predictButton = $("#predictButton")
         predictButton.on("click", function() {
 
@@ -102,23 +168,20 @@
 		/* CLEAR BUTTON */
 		var clearButton = $( "#clearButton" );
 		clearButton.on( "click", function(){
-			context.clearRect( 0, 0, 280, 280 );
-			context.fillStyle="white";
-			context.fillRect(0,0,canvas.width,canvas.height);
+			// context.clearRect( 0, 0, 280, 280 );
+			// context.fillStyle="white";
+			// context.fillRect(0,0,canvas.width,canvas.height);
+
+			clearCanvas(canvas,context)
 			
 			var tblResult = document.getElementById('tbl-result')
 			tblResult.style.display = "none"
 
 		});
 
-		$( "#colors" ).change(function(){
-			var color = $( "#colors" ).val();
-			context.color = color;
-		});		
-		
-		$( "#lineWidth" ).change(function(){
-			context.lineWidth = $( this ).val();
-		});
 	}
+
+	debug();
+	init()
 
 }());
