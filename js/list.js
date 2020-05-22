66fcd22bb7690f4840f2eb59668f461b0ef77c34
chalat.mo <ex-chalat.mo@cjdfintech.com>
2020-05-22 17:41:54 +0700

    feature delete data

--- js/list.js
+++ js/list.js
@@ -11,15 +11,22 @@
 	};
 	firebase.initializeApp(config);
 
-	var database = firebase.database();
+    var database = firebase.database();
+    var storageRef = firebase.storage().ref();
+
+    var url = []
+    var dataObj = []
+    var keyList = []
 
 	async function getUrlList(){
-		var url = []
+		
         await  database.ref('/thw/').once('value').then(function(snapshot) {
+            dataObj.push(snapshot.val())
 			snapshot.forEach(obj => {
-				url.push(obj.val().downloadURL);
+                // key.push(obj.val());
+                url.push(obj.val().downloadURL);
             });
-            console.log(url)
+            // console.log(url)
         });
 
 
@@ -36,6 +43,41 @@
         document.getElementById("show").innerHTML = "<p>" + "Data amount : " + url.length + "</p>";
 		
     }
+
+    var deleteButton = $( "#deleteButton" );
+    deleteButton.on( "click", function(){
+        var databasePath = '/thw/'
+
+        console.log("==========")
+        console.log("push key")
+    
+        dataObj.forEach(obj => {
+            Object.keys(obj).forEach(function(key) {
+                keyList.push(key)
+            });
+        })
+        // console.log(keyList)
+    
+        console.log("==========")
+        console.log("loop")
+    
+        keyList.forEach( key => {
+    
+            console.log("==========")
+            console.log(key)
+    
+            //Delete data
+            let databaseRef = database.ref(databasePath+key);
+            databaseRef.remove().then(function() {
+                console.log("Delete data : " + key)
+            });
+    
+            //Delete storage
+            storageRef.child(databasePath+key).delete().then(function() {
+                console.log("Delete file : " + key)
+            })
+        })
+    });
     
     
     getUrlList()
